7eda6593d94a0f86695eb4ca558027616294d62b
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-11-10 01:01:53 +0100

    work create works

--- app/Http/Controllers/Admin/WorkController.php
+++ app/Http/Controllers/Admin/WorkController.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Models\Artist;
+use App\Models\Work;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\StoreWorkRequest;
+use Illuminate\Support\Str;
+
 
 class WorkController extends Controller
 {
@@ -23,16 +27,29 @@ class WorkController extends Controller
      * Show the form for creating a new resource.
      */
     public function create()
-    {
-        //
+    {   
+        $user = Auth::user();
+        $artist = Artist::where('user_id', $user->id)->first();
+        return view('admin.works.create', compact('artist', 'user'));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreWorkRequest $request)
     {
-        //
+        $user = Auth::user();
+        $artist = Artist::where('user_id', $user->id)->first();
+        $validated_data = $request->validated();
+        $validated_data['artist_id'] = $artist->id;
+        $validated_data['slug'] = Str::slug($validated_data['title'], '-') . $user->id;
+
+        $newWork = Work::create($validated_data);
+
+        //dd($newWork);
+    
+        return redirect()->route('admin.works.index');
+        
     }
 
     /**
